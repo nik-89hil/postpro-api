ac78c780308570b4cd6daf48435c3e515494c73b
Nitin Kumar <nikhilkumar19072002@gmail.com>
2023-11-05 17:02:09 +0530

    apithree commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "https://widepost.onrender.com/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Methods",
         "GET,HEAD,OPTIONS,POST,PUT,DELETE"
